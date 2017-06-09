79a54e18800a71c4e141b85aa638090f0d0bff0a
Yu Gu <guyu13@mails.tsinghua.edu.cn>
2017-06-09 23:20:09 +0800

    Jun 9th commit

--- sdtw.py
+++ sdtw.py
@@ -10,10 +10,31 @@ def dump_feature(name, feature):
 def whiten(features, means, vars):
     pass
 
-def segmental_DTW(s1, s2):
+# returns several paths for future refinement (done by LCMA)
+def segmental_DTW(x, y):
     pass
 
+# returns the path defined by [(x1, y1), (x2, y2), ...]
+# as a subroutine of segmental_DTW
+def DTW(x, y, start):
+    pass
+
+# brute force search (O(NL)), instead of O(N log(L)) algorithm
+# returns a pair of left-inclusive indices
 def LCMA(S, L):
-    pass
+    n_samples = S.shape[0]
+    minimum_pair = (-1, -1)
+    current_minimum = 1e10
+    # l < 2L-1, see [27], lemma 7
+    for l in range(L, L*2):
+        for cursor in range(0, n_samples - l):
+            mean = np.mean(S[cursor:cursor+l])
+            if mean < current_minimum:
+                current_minimum = mean
+                minimum_pair = (cursor, cursor+l)
+    return minimum_pair
 
-    
+if __name__ == '__main__':
+    s = np.array([1,2,3,1,0,1,1,0,3])
+    p = LCMA(s, 3)
+    print(s[p[0]:p[1]])
--- segment.py
+++ segment.py
@@ -4,6 +4,8 @@ import numpy as np
 # WAV_FILE_PATH = 'data/audio.wav'
 WAV_FILE_PATH = 'data/short.wav'
 OUTPATH = 'data/segmented/'
+SILENCE_THRESHOLD = 42 # dB
+MINIMUN_SEGMENT_DURATION = 0.5 # sec
 
 def write_wav(out_path, S, sr):
     maxv = np.iinfo(np.int16).max
@@ -19,17 +21,23 @@ def gen_sample(wav_file):
 ''' devide the audio file into non-silent segments'''
 def segment(wav_file):
     wav, sr = rosa.core.load(wav_file, mono=True)
-    print(sr)
-    print(wav.shape)
-    intervals = rosa.effects.split(wav, top_db=50)
+    intervals = rosa.effects.split(wav, top_db=SILENCE_THRESHOLD)
+    n_samples = wav.shape[0]
     n_intervals = intervals.shape[0]
-    print(n_intervals, intervals.shape)
+    max_len, min_len = 0, 36000 * sr
     for i in range(n_intervals):
         start, end = intervals[i]
-        print(start, end)
-        write_wav('%s%d.wav'%(OUTPATH, i), wav[intervals[i][0]:intervals[i][1]], sr)
+        duration = end - start
+        max_len = max(duration, max_len)
+        min_len = min(duration, min_len)
+        if duration/sr > MINIMUN_SEGMENT_DURATION:
+            write_wav('%s%d.wav'%(OUTPATH, i), wav[intervals[i][0]:intervals[i][1]], sr)
+        else:
+            write_wav('%s_%d.wav'%(OUTPATH, i), wav[intervals[i][0]:intervals[i][1]], sr)
+    
+    print('divided into %d segments, max_length=%fs, min_length=%fs, avg=%fs'
+             % (n_intervals, max_len/sr, min_len/sr, n_samples/sr/n_intervals))
 
 if __name__ == '__main__':
     segment(WAV_FILE_PATH)
-    # gen_sample(WAV_FILE_PATH)
 
