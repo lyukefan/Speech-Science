f85169b6d3f8cd684eadcf0ec00d5568ac77c291
Yu Gu <guyu13@mails.tsinghua.edu.cn>
2017-06-10 14:38:58 +0800

    Segmental_DTW implemented (not so neat, though)

---
+++ config.py
@@ -0,0 +1,7 @@
+# WAV_FILE_PATH = 'data/audio.wav'
+WAV_FILE_PATH = 'data/short.wav'
+SEGMENTED_PATH = 'data/segmented/'
+SILENCE_THRESHOLD = 42 # dB
+MINIMUN_SEGMENT_DURATION = 0.5 # sec
+
+INFINITY = 1E10
